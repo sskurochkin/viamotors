d6dd965c84a86f2bae7dc9a7a40fb32b05eddbd5
s.kurochkin <semenkurochkin@gmail.com>
2022-10-10 22:50:20 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./js/common.js";
+import "./assets/css/main.css";
+import "./assets/scss/main.scss";
+
+import './js/burger.js'
+import './js/to-list.js'
+
+//
+// if (process.env.NODE_ENV !== `production`) {
+//   async function load() {
+//     let resize_info = await import("./smart-gread-layer/smart-gread-layer");
+//     resize_info.default();
+//   }
+//
+//   load();
+// }
